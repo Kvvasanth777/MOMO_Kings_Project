e204ead0a444858cff6741665876e3f38d3688d7
Ubuntu <ubuntu@ip-172-31-12-122.ap-south-1.compute.internal>
2026-09-21 16:23:27 +0000

    Rename DockerFile to Dockerfile

--- restaurant_management/restaurant_management/settings.py
+++ restaurant_management/restaurant_management/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME', 'momokings_db'),
         'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'momo-kings'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
